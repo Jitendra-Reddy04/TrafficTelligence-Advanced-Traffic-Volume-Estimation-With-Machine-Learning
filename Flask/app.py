2943d18111e82db4a62f31cc6d2863686fc916c6
Palagiri Jitendra Reddy <jithendrar737@gmail.com>
2025-06-25 10:27:24 +0530

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -12,52 +12,72 @@
 #     name: conda-base-py
 # ---
 
-# %%
-import numpy as np
+import numpy as np 
 import pickle
-import joblib
-import matplotlib.pyplot as plt
-import time
-import pandas
+import matplotlib.pyplot as plt 
+import time 
+import pandas 
 import os
 from flask import Flask, request, jsonify, render_template
 
-app=Flask(__name__)
-model=pickle.load(open("model.pkl",'rb'))
-scale = pickle.load(open('encoder.pkl','rb'))
+app = Flask(__name__)
+
+# Load model and encoder
+with open(r"C:\Users\madhu\madhu aiml\.ipynb_checkpoints\flask\model.pkl", 'rb') as file:
+    model = pickle.load(file)
+
+with open(r"C:\Users\madhu\madhu aiml\.ipynb_checkpoints\flask\encoder.pkl", 'rb') as file:
+    scale = pickle.load(file)
+print("Model's feature names:", model.feature_names_in_)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=["POST", "GET"])
+def predict():
+    print("Form data:", request.form)
+    
+    # Create a dictionary to store form values, converting keys to lowercase
+    form_data = {key.lower(): float(value) for key, value in request.form.items()}
+    print("Processed form data:", form_data)
+    
+    # Use the model's feature names
+    feature_names = model.feature_names_in_
+    print("Model's feature names:", feature_names)
+    
+    # Create input_feature list in the correct order
+    input_feature = []
+    for feature in feature_names:
+        if feature in form_data:
+            input_feature.append(form_data[feature])
+        else:
+            print(f"Warning: {feature} not found in form data")
+            input_feature.append(0)  # or some default value
+    
+    print("Number of input features:", len(input_feature))
+    print("Input features:", input_feature)
+    
+    # Create DataFrame
+    data = pandas.DataFrame([input_feature], columns=feature_names)
+    print("DataFrame:", data)
+    
+    # Make prediction
+    prediction = model.predict(data)
+    
+    text = "Estimated Traffic Volume is: "
+    return render_template("output.html",result = text + str(prediction) + "units")
+
+if __name__ == "__main__":
+    print("Model's feature names:", model.feature_names_in_)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(port=port, debug=True, use_reloader=False)
+
+
+# %%
 
 @app.route("/")
-def home():
-    return render_template("home.html")
 
-@app.route("/predict")
-def predict():
-    return render_template("web_page.html")
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-@app.route("/result",methods=["POST"])
-def result():
-    input_feature=[float(x)for x in request.form.values()]
-    features_values=[np.array(input_feature[0:11])]
-    # features = features_values[:7]
-    # print(features)
-    names = [['holiday','temp','rain','snow','weather','year','month','day','hours','minutes','seconds']]
-    data = pandas.DataFrame(features_values, columns=names)
-    # data = scale.fit_transform(data)
-    # data = pandas.DataFrame(data, columns = names)
-    prediction=model.predict(data)
-    print(prediction)
-    text = "Estimated Traffic Volume is :"
-    return render_template("result.html" ,prediction_text = text + str(prediction))
-
-
-if __name__=="__main__":
-    # port=8000, debug=True)
-   
-    app.run(debug=True,use_reloader=False)
 # * running the app
 
 # %%
